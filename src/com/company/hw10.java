723968992c950efa23430f3a1109c3d33fa3c80a
lunarsoftware <diana.umudova@gmail.com>
2020-04-18 16:12:46 +0300

    hw10

---
+++ src/com/company/hw10.java
@@ -0,0 +1,27 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+public class hw10 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Введите размерность матрицы (столбец и строка):");
+
+        BufferedReader s = new BufferedReader(new InputStreamReader(System.in));
+        int y = Integer.parseInt(s.readLine());
+        int x = Integer.parseInt(s.readLine());
+        int massive[][] = new int[y][x];
+        int i, j = 0;
+        for (i = 0; i < y; i++) {
+            for (j = 0; j < x; j++) {
+                System.out.println("Введите число в массив:");
+                BufferedReader num = new BufferedReader(new InputStreamReader(System.in));
+                int number = Integer.parseInt(num.readLine());
+                massive[i][j] = number;
+            }
+        }
+        System.out.println("Первая строчка массива, умноженная на 3:");
+        for (j = 0; j < x; j++) {
+                       System.out.print(massive[0][j]*3+" ");
+        }
+    }
+}
