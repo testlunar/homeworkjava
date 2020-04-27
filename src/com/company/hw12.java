d9b907f88040677414b457541fde817562061892
lunarsoftware <diana.umudova@gmail.com>
2020-04-27 22:47:09 +0300

    hw12

---
+++ src/com/company/hw12.java
@@ -0,0 +1,18 @@
+package com.company;
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+
+public class hw12 {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String S = reader.readLine();
+               String[] sParts = S.split(" ");
+        for (int i=0; i<sParts.length;i++){
+        System.out.print(sParts[i]);
+        }
+    }
+}
