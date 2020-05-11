5fe81c4cda33ea9c55863bbb360831902e2b6c5a
lunarsoftware <diana.umudova@gmail.com>
2020-05-11 23:18:50 +0300

    final3

---
+++ src/com/company/final3.java
@@ -0,0 +1,21 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.ArrayList;
+
+public class final3 {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Введите текущий курс доллара к рублю:");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        double s = Double.parseDouble(reader.readLine());
+        System.out.println("Введите сумму в рублях:");
+        int sum = Integer.parseInt(reader.readLine());
+        double result = (double)sum/s;
+        System.out.println("Результат: "+ new BigDecimal(result).setScale(2, RoundingMode.HALF_UP).floatValue());
+    }
+}
+
+
