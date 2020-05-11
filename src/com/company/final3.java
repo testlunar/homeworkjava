96b049e18dac437e4a805536976043c025f7d34d
lunarsoftware <diana.umudova@gmail.com>
2020-05-11 23:38:56 +0300

    final3

--- src/com/company/final3.java
+++ src/com/company/final3.java
@@ -4,7 +4,7 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.ArrayList;
+
 
 public class final3 {
     public static void main(String[] args) throws Exception {
